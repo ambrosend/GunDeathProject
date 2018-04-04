9628aa4a404ef6974880dbe6463eb09fd1c0995b
Jensen Shurbert <shurbertj@kenyon.edu>
2018-04-03 20:03:42 -0400

    Added multiple visualizations

--- guns.js
+++ guns.js
@@ -4,31 +4,23 @@ var title;
 console.log("Ready!");
 $(document).ready(function () {
 	AutoRefresh() ;
-//    $("#clear").click(clearResults);
-
- //    $(".dropdown-menu li a").click(function(){
-// 	console.log("pick!"+$(this).text());
-// 	$(this).parents(".btn-group").find('.selection').text($(this).text());
-// 	title=$(this).text();
-//         var fileName=$(this).attr("data-file");
-// 	console.log(fileName);
-// 	makechart(fileName,title);
-//     });
+	makechart();
  });
 
-function makechart(filename) {
+function makechart() {
     var data;
     $.ajax({
 	type: "GET",
-	url: filename,
+	url: datafile,
 	dataType: "text",
 	success: function(response)
 	{
-	    //console.log(response);
-	    data = $.csv.toArrays(response);
-	    var chartData = prepData(data);
-	    //console.log(chartData);
-	    showChart(chartData);
+		console.log(response+$.csv);
+	    var data = $.csv.toArrays(response);
+	    console.log(JSON.stringify(data));
+	    
+	    //var chartData = prepData(data);
+	    //showChart(chartData);
 	}
     });
 }
@@ -51,9 +43,7 @@ function prepData(data) {
 }
 
 
-
-
-// From HighCharts demo
+// From HighCharts demo for donut visualizations
 var colors = Highcharts.getOptions().colors,
     categories = [
         "None Selected",
@@ -69,7 +59,181 @@ var colors = Highcharts.getOptions().colors,
             "drilldown": {
                 "name": "None Selected",
                 "categories": [
+                    "Unknown1",
+                    "Male",
+                    "Female"
+                ],
+                "data": [
+                    25.00,
+                    21.42,
+                    3.58
+                ]
+            }
+        },
+        {
+            "y": 31.34,
+            "color": colors[4],
+            "drilldown": {
+                "name": "Suicide",
+                "categories": [
                     "Unknown",
+                    "Male",
+                    "Female"
+                ],
+                "data": [
+                    15.67,
+                    13.52,
+                    2.15
+                ]
+            }
+        },
+        {
+            "y": 17.45,
+            "color": colors[0],
+            "drilldown": {
+                "name": "Homicide",
+                "categories": [
+                    "Unknown",
+                    "Male",
+                    "Female"
+                ],
+                "data": [
+                    8.72,
+                    7.39,
+                    1.34
+                ]
+            }
+        },
+        {
+            "y": 0.81,
+            "color": colors[3],
+            "drilldown": {
+                "name": "Accident",
+                "categories": [
+                    "Unknown",
+                    "Male",
+                    "Female"
+                ],
+                "data": [
+                    0.41,
+                    0.35,
+                    0.05
+                ]
+            }
+        },
+        {
+            "y": 0.40,
+            "color": colors[4],
+            "drilldown": {
+                "name": "Unknown",
+                "categories": [
+                    "Unknown",
+                    "Male",
+                    "Female"
+                ],
+                "data": [
+                    0.20,
+                    0.16,
+                    0.04
+                ]
+            }
+        },
+    ],
+    data2 = [
+        {
+            "y": 50.00,
+            "color": colors[2],
+            "drilldown": {
+                "name": "None Selected",
+                "categories": [
+                    "Unknown2",
+                    "Male",
+                    "Female"
+                ],
+                "data": [
+                    25.00,
+                    21.42,
+                    3.58
+                ]
+            }
+        },
+        {
+            "y": 31.34,
+            "color": colors[4],
+            "drilldown": {
+                "name": "Suicide",
+                "categories": [
+                    "Unknown",
+                    "Male",
+                    "Female"
+                ],
+                "data": [
+                    15.67,
+                    13.52,
+                    2.15
+                ]
+            }
+        },
+        {
+            "y": 17.45,
+            "color": colors[0],
+            "drilldown": {
+                "name": "Homicide",
+                "categories": [
+                    "Unknown",
+                    "Male",
+                    "Female"
+                ],
+                "data": [
+                    8.72,
+                    7.39,
+                    1.34
+                ]
+            }
+        },
+        {
+            "y": 0.81,
+            "color": colors[3],
+            "drilldown": {
+                "name": "Accident",
+                "categories": [
+                    "Unknown",
+                    "Male",
+                    "Female"
+                ],
+                "data": [
+                    0.41,
+                    0.35,
+                    0.05
+                ]
+            }
+        },
+        {
+            "y": 0.40,
+            "color": colors[4],
+            "drilldown": {
+                "name": "Unknown",
+                "categories": [
+                    "Unknown",
+                    "Male",
+                    "Female"
+                ],
+                "data": [
+                    0.20,
+                    0.16,
+                    0.04
+                ]
+            }
+        },
+    ],
+    data3 = [
+        {
+            "y": 50.00,
+            "color": colors[2],
+            "drilldown": {
+                "name": "None Selected",
+                "categories": [
+                    "Unknown3",
                     "Male",
                     "Female"
                 ],
@@ -151,13 +315,25 @@ var colors = Highcharts.getOptions().colors,
     ],
     browserData = [],
     versionsData = [],
+    // browserData2 = [],
+//     versionsData2 = [],
+//     browserData3 = [],
+//     versionsData3 = [],
     i,
     j,
     dataLen = data.length,
+//     dataLen2 = data2.length,
+//     dataLen3 = data3.length,
+
     drillDataLen,
     brightness;
+    
+    processData1(data);
+    // processData2(data2);
+//     processData3(data3);
+	
 
-
+function processData1(data) {
 // Build the data arrays
 for (i = 0; i < dataLen; i += 1) {
 
@@ -179,14 +355,64 @@ for (i = 0; i < dataLen; i += 1) {
         });
     }
 }
+}
+// function processData2(data) {
+// // Build the data arrays
+// for (i = 0; i < dataLen2; i += 1) {
+// 
+//     // add browser data
+//     browserData2.push({
+//         name: categories[i],
+//         y: data2[i].y,
+//         color: data2[i].color
+//     });
+// 
+//     // add version data
+//     drillDataLen = data2[i].drilldown.data2.length;
+//     for (j = 0; j < drillDataLen; j += 1) {
+//         brightness = 0.2 - (j / drillDataLen) / 5;
+//         versionsData2.push({
+//             name: data2[i].drilldown.categories[j],
+//             y: data2[i].drilldown.data2[j],
+//             color: Highcharts.Color(data[i].color).brighten(brightness).get()
+//         });
+//     }
+// }
+// }
+// 
+// function processData3(data) {
+// // Build the data arrays
+// for (i = 0; i < dataLen3; i += 1) {
+// 
+//     // add browser data
+//     browserData3.push({
+//         name: categories[i],
+//         y: data3[i].y,
+//         color: data3[i].color
+//     });
+// 
+//     // add version data
+//     drillDataLen = data3[i].drilldown.data3.length;
+//     for (j = 0; j < drillDataLen; j += 1) {
+//         brightness = 0.2 - (j / drillDataLen) / 5;
+//         versionsData3.push({
+//             name: data3[i].drilldown.categories[j],
+//             y: data3[i].drilldown.data[j],
+//             color: Highcharts.Color(data[i].color).brighten(brightness).get()
+//         });
+//     }
+// }
+// }
 
-// Create the chart
-Highcharts.chart('container', {
+
+
+// Create the chart for donut visualizations
+Highcharts.chart('container1', {
     chart: {
         type: 'pie'
     },
     title: {
-        text: 'Gun Deaths in the United States in 2016'
+        text: 'Gun Deaths: Intent and Gender'
     },
     subtitle: {
         text: 'Source: <a href="http://statcounter.com" target="_blank">statcounter.com</a>'
@@ -246,6 +472,145 @@ Highcharts.chart('container', {
         }]
     }
 });
+
+// Create the chart for donut visualizations
+Highcharts.chart('container2', {
+    chart: {
+        type: 'pie'
+    },
+    title: {
+        text: 'Gun Deaths: Intent and Gender'
+    },
+    subtitle: {
+        text: 'Source: <a href="http://statcounter.com" target="_blank">statcounter.com</a>'
+    },
+    yAxis: {
+        title: {
+            text: 'Total percent market share'
+        }
+    },
+    plotOptions: {
+        pie: {
+            shadow: false,
+            center: ['50%', '50%']
+        }
+    },
+    tooltip: {
+        valueSuffix: '%'
+    },
+    series: [{
+        name: 'Intent',
+        data: browserData,
+        size: '60%',
+        dataLabels: {
+            formatter: function () {
+                return this.y > 5 ? this.point.name : null;
+            },
+            color: '#ffffff',
+            distance: -30
+        }
+    }, {
+        name: 'Gender',
+        data: versionsData,
+        size: '80%',
+        innerSize: '60%',
+        dataLabels: {
+            formatter: function () {
+                // display only if larger than 1
+                return this.y > 1 ? '<b>' + this.point.name + ':</b> ' +
+                    this.y + '%' : null;
+            }
+        },
+        id: 'versions'
+    }],
+    responsive: {
+        rules: [{
+            condition: {
+                maxWidth: 400
+            },
+            chartOptions: {
+                series: [{
+                    id: 'versions',
+                    dataLabels: {
+                        enabled: false
+                    }
+                }]
+            }
+        }]
+    }
+});
+
+
+// Create the chart for donut visualizations
+Highcharts.chart('container3', {
+    chart: {
+        type: 'pie'
+    },
+    title: {
+        text: 'Gun Deaths: Intent and Gender'
+    },
+    subtitle: {
+        text: 'Source: <a href="http://statcounter.com" target="_blank">statcounter.com</a>'
+    },
+    yAxis: {
+        title: {
+            text: 'Total percent market share'
+        }
+    },
+    plotOptions: {
+        pie: {
+            shadow: false,
+            center: ['50%', '50%']
+        }
+    },
+    tooltip: {
+        valueSuffix: '%'
+    },
+    series: [{
+        name: 'Intent',
+        data: browserData,
+        size: '60%',
+        dataLabels: {
+            formatter: function () {
+                return this.y > 5 ? this.point.name : null;
+            },
+            color: '#ffffff',
+            distance: -30
+        }
+    }, {
+        name: 'Gender',
+        data: versionsData,
+        size: '80%',
+        innerSize: '60%',
+        dataLabels: {
+            formatter: function () {
+                // display only if larger than 1
+                return this.y > 1 ? '<b>' + this.point.name + ':</b> ' +
+                    this.y + '%' : null;
+            }
+        },
+        id: 'versions'
+    }],
+    responsive: {
+        rules: [{
+            condition: {
+                maxWidth: 400
+            },
+            chartOptions: {
+                series: [{
+                    id: 'versions',
+                    dataLabels: {
+                        enabled: false
+                    }
+                }]
+            }
+        }]
+    }
+});
+
+
+
+
 
 var x=0;
 function displaydeaths() {
